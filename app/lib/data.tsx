73712860debab9f9df39045d7ce602067d5dc8f5
Kate <kate.nordqvist@gmail.com>
2024-02-04 21:15:44 +0100

    update data

---
+++ app/lib/data.tsx
@@ -0,0 +1,53 @@
+import { logoInfo } from './definitions';
+
+export const logoArray: logoInfo[] = [
+	{
+		alt: 'javascript logo',
+		image: '/javascript-logo.svg',
+		name: 'javascript',
+	},
+	{
+		alt: 'react logo',
+		image: '/react-logo.svg',
+		name: 'react',
+	},
+	{
+		alt: 'typescript logo',
+		image: '/typescript-logo.png',
+		name: 'typescript',
+	},
+	{
+		alt: 'node logo',
+		image: '/node-logo.svg',
+		name: 'node',
+	},
+	{
+		alt: 'mongodb logo',
+		image: '/mongodb-logo.svg',
+		name: 'mongodb',
+	},
+	{
+		alt: 'mysql logo',
+		image: '/mysql-logo.svg',
+		name: 'mysql',
+	},
+	{
+		alt: 'firebase logo',
+		image: '/firebase-logo.png',
+		name: 'firebase',
+	},
+];
+
+export const techStackArray = [
+	'Javascript',
+	'Typescript',
+	'React',
+	'React Native',
+	'Node.js',
+	'Firebase',
+	'MySQL',
+	'mongoDB',
+];
+
+export const aboutText =
+	"Hi!\nI'm Kate, a junior Frontend developer searching for new ways to challenge myself.\nThroughout my education, I have particularly enjoyed working with React/React-Native, Node.js, and Express. I have been drawn towards the full-stack possibilities that these technologies offer. In my free time, I often engage in different projects and courses to gain a deeper understanding of how things work.\n\nIf you visit my Github you can see that my activity contribution is filled with beautiful green dots, some would say it is because I'm a nerd, and I would agree.\nMy projects may not be the most beautiful regarding design, but I've poured my heart into making them functional and trying to learn best practices within the area.\n\nMy experience working with diverse groups of people has taught me the importance of effective communication and building positive relationships with clients and colleagues.\nIn my previous roles I developed a profound understanding of the significance of strong teamwork. Both as a teacher and an animal caretaker, I have honed the ability to work methodically and document my work clearly, which has proven to be a strength in my programming development. I have received multiple positive feedback for my adherence to clean code standards.\n\nI am confident that my passion for development, my willingness to take on challenges, and my commitment to continuous learning would make me a valuable asset to your team.\n\nBest regards, Kate Nordqvist";
