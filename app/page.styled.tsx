9d324075043391db67b997e690b88001c79d743d
Kate <kate.nordqvist@gmail.com>
2024-02-04 21:14:59 +0100

    fix scroll

--- app/page.styled.tsx
+++ app/page.styled.tsx
@@ -3,5 +3,40 @@ import styled from 'styled-components';
 
 export const StyledMainContainer = styled.div`
 	display: flex;
-	min-height: 100vh;
+	flex-direction: column;
+	min-height: 200vh;
+
+	& .arrowDown,
+	.arrowUp {
+		position: absolute;
+		display: flex;
+		align-self: flex-end;
+		width: 74px;
+		height: 74px;
+		left: 50vw;
+		fill: #f9ead984;
+		cursor: pointer;
+
+		& :hover {
+			fill: #f9ead9;
+		}
+	}
+
+	& .arrowUp {
+		align-self: flex-start;
+	}
+`;
+
+export const FirstContainer = styled.div`
+	display: flex;
+	height: 100vh;
+`;
+export const SecondContainer = styled.div`
+	display: flex;
+	height: 100vh;
+`;
+
+export const Logos = styled.div`
+	display: flex;
+	align-self: flex-end;
 `;
