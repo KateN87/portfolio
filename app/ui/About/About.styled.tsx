57a1a824e2e66fd2fe0b0668af0525822fba008b
Kate <kate.nordqvist@gmail.com>
2024-02-04 21:15:17 +0100

    add about

---
+++ app/ui/About/About.styled.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const StyledMainContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	height: 60vh;
+	width: 50vw;
+	align-self: center;
+	margin: auto;
+
+	& p {
+		font-size: 18px;
+		margin-top: 15px;
+	}
+`;
